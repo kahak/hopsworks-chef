f248abfad90c7e5d0ae27df492c4862434a141a7
Alex Ormenisan <aaor@kth.se>
2016-08-25 10:20:48 +0200

    add dela to vagrant installation with temp hack for udp port forwarding

--- run-vagrant.sh
+++ run-vagrant.sh
@@ -11,4 +11,7 @@ berks vendor cookbooks
 echo "Running the Vagrantfile using 'vagrant up'"
 nohup vagrant up &
 
-
+sleep 1m
+echo "abc" | nc -u 193.10.64.11 40100
+echo "abc" | nc -u 193.10.64.11 40200
+echo "abc" | nc -u 193.10.64.11 40300
